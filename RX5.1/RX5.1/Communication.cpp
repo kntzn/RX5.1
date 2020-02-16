ca0bfab71fd05777e157dc8671f7f9ec2f422c8f
kntzn <kntznart@gmail.com>
2020-02-16 21:17:09 +0300

    [Add]: send&receive packet functions

--- RX5.1/RX5.1/Communication.cpp
+++ RX5.1/RX5.1/Communication.cpp
@@ -3,6 +3,70 @@
 // 
 
 #include "Communication.h"
+#include <stdint.h>
+#include <stddef.h>
+
+#define StartBlock()	(code_ptr = dst++, code = 1)
+#define FinishBlock()	(*code_ptr = code)
+
+void Communication::sendPacket(uint8_t* pack, size_t len)
+	{
+	assert(len <= PACK_SIZE_MAX);
+
+	uint8_t cobsEncodedPack[PACK_SIZE_MAX + 1] = {};
+	size_t cobsEncodedPackLen =
+		COBS::encode(pack, len, cobsEncodedPack);
+
+	// Actual packet
+	Serial.write(cobsEncodedPack, cobsEncodedPackLen);
+	// EOP byte
+	Serial.write(EOP);
+	}
+
+size_t Communication::receivePacket(uint8_t * pack)
+	{
+	// Reads the serial
+	while (Serial.available())
+		inputBuf.push_back(Serial.read());
+
+	// Searches for the EOP symb.
+	int eop = -1;
+	for (int i = 0; i < inputBuf.size(); i++)
+		if (inputBuf[i] == EOP)
+			{
+			eop = i;
+			break;
+			}
+
+	// Decodes the message if it is avail.
+	// Fix: prevents corrupted packets being decoded 
+	//              out of range of the "pack" array
+	size_t cobsDecodedPackLen = 0;
+	if (eop != PACK_SIZE_MAX + 1)
+		{
+		cobsDecodedPackLen =
+			COBS::decode(inputBuf.data(), eop, pack);
+		}
+
+	// Removes the message from the buffer
+	if (eop != -1)
+		{
+		size_t initial_size = inputBuf.size();
+
+		uint8_t* data = inputBuf.data();
+
+		// Buffer shift
+		for (int i = 0; i < initial_size - eop - 1; i++)
+			data[i] = data[i + eop + 1];
+
+		for (int i = 0; i < eop + 1; i++)
+			inputBuf.pop_back();
+
+		}
+
+	return cobsDecodedPackLen;
+	}
+
 
 Communication::Communication(uint8_t set_pin, uint8_t channel) :
 	setPin(set_pin),
--- RX5.1/RX5.1/Communication.h
+++ RX5.1/RX5.1/Communication.h
@@ -10,15 +10,48 @@
 #endif
 
 #include "cobs.h"
+#include "Sarray.h"
+
+#define PACK_SIZE_MAX 64
+#define PACK_SIZE_DEFAULT 4
+#define EOP 0x00
+
+#define REQUEST_PERIOD 500
+#define RESPONSE_TIMEOUT 100
+#define REQUEST_TIMEOUT 500
+#define RESPONSE_PACKETS 5
 
 class Communication
 {
  protected:
 
+	 sarray <uint8_t, PACK_SIZE_MAX + 1> inputBuf;
+	 uint8_t buffer [PACK_SIZE_MAX];
 
 	 uint8_t channelId, setPin;
  public:
 	 Communication(uint8_t set_pin, uint8_t channel);
+
+	 void changeCh(uint8_t ch);
+
+	 // TODO:
+
+	 //void sendCommand(command cmd, uint16_t arg = 0);
+	 //void sendRequest(command req);
+	 //void sendResponse(response resp, uint16_t val = 0);
+
+	 //command receiveRequest();
+	 //response receiveResponse();
+
+	 void sendPacket(uint8_t* pack, size_t len);
+	 size_t receivePacket(uint8_t* pack);
+
+	 void activateRawinput();
+	 void flush();
+
+	 bool rawinputActive();
+	 // Returns ptr to the args. array
+	 uint8_t *argbuf();
 
 };
 
